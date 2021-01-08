e0c18c17768cedcdfc6933750bd30abbd1b96a37
katebekk <44308249+katebekk@users.noreply.github.com>
2021-01-08 17:27:00 +1000

    Update 5task.py

--- 5task.py
+++ 5task.py
@@ -1,44 +1,58 @@
-# Про каждое блюдо известен набор продуктов, требуемый для его изготовления, и количество каждого продукта.
-# Задан список продуктов, имеющихся в наличие: какой продукт, сколько.
-# Написать функции, формирующие возможное меню. Учитывать, что суммарное количество одного и того же продукта 
-# в меню не может превышать имеющееся количество продукта.
+; Про каждое блюдо известен набор продуктов, требуемый для его изготовления, и количество каждого продукта.
+; Задан список продуктов, имеющихся в наличие: какой продукт, сколько.
+; Написать функции, формирующие возможное меню. Учитывать, что суммарное количество одного и того же продукта 
+; в меню не может превышать имеющееся количество продукта.
 
-dishes = [["Борщ", [["Картофель", 2],["Капуста", 1],["Свекла", 1],["Морковь", 1],["Фасоль", 1]]],
-["Плов", [["Рис", 100],["Морковь", 1],["Чеснок", 1],["Грибы", 100]]],
-["Ризотто",[["Рис", 100],["Сливки", 100],["Чеснок", 1],["Грибы", 100]]],
-["Пицца", [["Помидор", 2],["Тесто", 200],["Сыр", 100]]],
-["Щи", [["Картофель", 2],["Капуста", 1],["Свекла", 1],["Морковь", 1]]]]
+(setq inStock '(("Макароны" 2)("Колбаса" 5) ("Сыр" 2) ("Тесто" 2)("Вренье" 5)("Картофель" 9)("Хлеб" 1)))
+ 
+(setq recept '(("Пицца" (("Колбаса" 2) ("Сыр" 1) ("Тесто" 1))) ("Сосиска в тесте" (("Колбаса" 1) ("Тесто" 1))) ("Карбонара" (("Колбаса" 2) ("Сыр" 1) ("Макароны" 1)))))
+ 
 
-inStock = [["Картофель", 10],["Капуста", 5],["Свекла", 6],["Морковь", 7],["Фасоль", 3],["Рис", 200],["Сливки", 100],["Чеснок", 1],["Грибы", 200]]
+ (defun have_products ((availinStock productOfRecipe)
+(cond
+    ((NULL (availinStock) T)
+    ((>= (CADAR (availinStock) (findProductCost (CAAR (availinStock) productOfRecipe)) (have_products(CDR (availinStock) productOfRecipe))
+    (T NIL)
+)
+)
+ 
+(defun make_menu (availinStock recipes)
+(cond
+    ((NULL recipes) NIL)
+    ((eq T (have_products (availinStock (CADAR recipes))) (cons (CAAR recipes) (make_menu (dif availinStock (CADAR recipes)) (CDR recipes))))
+    (T (make_menu availinStock (CDR recipes)))
+)
+)
 
-# Написать функции, формирующие возможное меню. Учитывать, что суммарное количество одного и того же продукта 
-# в меню не может превышать имеющееся количество продукта.
-
-def findProdact(prodact, inSt):
-    for st in inSt:
-        if(prodact[0] == st[0]):
-            return inSt.index(st)
-def delProd(prodact, inSt):
-    ind = findProdact(prodact, inSt)
-    if(inSt[ind][1] > prodact[1]):
-        inSt[ind][1] = inSt[ind][1] - prodact[1]
-    else: inSt.remove(inSt[ind])
-    
-def makeMenu(Stock):
-    St = Stock
-    menu = []
-    for d in dishes:
-        isAll = 0
-        for prod in d[1]:
-            if(findProdact(prod , St) != None):
-                isAll = isAll + 1
-                delProd(prod, St)
-        if(isAll == len(d[1])):
-            menu.append(d[0])
-    
-    return menu        
-            
-            
-print(makeMenu(inStock))
-
-
+(defun findProductCost (product source)
+(cond
+    ((NULL source) 0)
+    ((string-equal product (CAAR source)) (CADAR source))
+    (T (findProductCost product (CDR source)))
+)
+)
+(defun dif (availProduct productOfRecipe)
+(cond
+    ((NULL availProduct) NIL)
+    (T (cons (list (CAAR availProduct) (- (CADAR availProduct) (findProductCost (CAAR availProduct) productOfRecipe))) (dif (CDR availProduct) productOfRecipe)))
+)
+)
+(defun allMenu (products recipes)
+(cond
+    ((NULL recipes) NIL)
+    (T (cons (make_menu products (CAR recipes)) (allMenu products (CDR recipes))))
+)
+)
+(defun comb (recipes)
+    (cond
+        ((NULL (CDR recipes)) NIL)
+        (T (cons (list (CAR recipes) (CADR recipes)) (comb (cons (CAR recipes) (CDDR recipes)))))
+    )
+)
+(defun all-comb (recipes)
+(cond
+    ((NULL recipes) NIL)
+    (T (cons (comb recipes) (all-comb (CDR recipes))))
+)
+)
+(print(allMenu inStock recept))
