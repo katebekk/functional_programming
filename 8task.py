c91472b15fc344cebdc01ea3a44e68f95a92e6b8
katebekk <44308249+katebekk@users.noreply.github.com>
2021-01-08 16:58:10 +1000

    Update 8task.py

--- 8task.py
+++ 8task.py
@@ -8,99 +8,114 @@
 # 5) Пересечение 3х множеств
 # 6) Вывод на экран элементов множества в порядке возрастания
 
-list = []
-
-def find(el, L):
-    for l in L:
-        if(l == el):
-            return L.index(l)
-
+list1 = [6,1,1,2,3,11,6,1]
+list2 = [8,9,1]
 # 1) Добавление заданных элементов
-def addToList(L, new_el):
-    for el in new_el:
-        L.append(el)
-    return L
+def add(list1,list2):
+    list3 = list1.copy()
+    return list3+list2
+print(add(list1,list2))
 
-# 2)Удаление заданных элементов
-def delInList(L, del_el):
-    for el in del_el:
-        if(find(el, L) != None):
-            L.remove(el)
-    return L
+# 2) Удаление заданных элементов
+def del_numb(list1, num):
+    if(len(list1) == 1):
+      if(list1.count(num) > 0):
+            list1.remove(num) 
+            return list1
+    else:
+        if(list1.count(num) > 0):
+            ind = list1.index(num) 
+            list1.remove(num)
+            return del_numb(list1,num)
+
+def del_list(list1,list2):
+    if(len(list2) == 1):
+        if(list1.count(list2[0]) > 0):
+            del_numb(list1,list2[0])
+            return list1
+    else:
+        if(list1.count(list2[0]) > 0):
+            del_numb(list1,list2[0])
+        return del_list(list1,list2[1:])
+print(del_list(list1,list2))
+del_numb(list1,1)
 
 # 3) Проверка на принадлежность
-def isInList(L, els):
-    isHere = true
-    for el in els:
-        if(find(el, L) == None):
-            isHere = false
-    return isHere
+def inList(list1, list2):
+    print(list2)
+    b = True
+    if(len(list2) == 1):
+        if(list1.count(list2[0]) == 0):
+            b = False
+            return b
+    else:
+        if(list1.count(list2[0]) == 0):
+            b = False
+        b = inList(list1, list2[1:])
+    return b
+    
+l1 = [3,4,5,11,2]
+l2 = [2,3]
+print(inList(l1,l2))
 
 # 4) Объеденение 3х множеств
-def union(L1, L2, L3):
-    L = []
-    for el in L1:
-        if(find(el, L) == None):
-            L.append(el)
-    for el in L2:
-        if(find(el, L) == None):
-            L.append(el)
-    for el in L3:
-        if(find(el, L) == None):
-            L.append(el)
-    return L
+def union2(list1,list2):
+    if(len(list2) == 1):
+       if(list1.count(list2[0]) == 0): 
+           list1.append(list2[0])
+           return list1
+    else:
+        if(list1.count(list2[0]) == 0):
+           list1.append(list2[0])
+        union2(list1, list2[1:])
+    return list1
     
-print(union([1,2,4],[2,5,17],[12,1,0]))
+def union3(list1,list2,list3):
+    return union2(union2(list1,list2),list3)
+    
+L1 = [3,4,5,11,2]
+L2 = [2,6]
+L3 = [2,3,8,9]
+print("объединение :", union3(L1, L2,L3))
 
 # 5) Пересечение 3х множеств
-def cross(L1, L2, L3):
-    L = L1
-    for el in L:
-        if(find(el, L2) == None):
-            L.remove(el)
-    for el in L:
-        if(find(el, L3) == None):
-            L.remove(el)
+def cross(l1,l2,l3):
+    if(len(l2) == 1):
+        if(l1.count(l2[0]) != 0): 
+            l3.append(l2[0])
+            return l3
+    else:
+        if(l1.count(l2[0]) != 0):
+            l3.append(l2[0])
+        cross(l1,l2[1:],l3)
+    return l3
 
-    return L
+ll = []
+def cross3(l1,l2,l3):
+    ll = []
+    l = cross(l1,l2, ll)
+    ll =[]
+    return cross(l,l3,ll)
+    
+L1 = [3,4,5,11,2,6]
+L2 = [2,6,8]
+L3 = [2,3,8,9,6]
 
-
-print(cross([1,2,4],[2,5,17,1],[12,1,0,2]))
+print("пересечение :", cross3(L1, L2,L3))
 
 # 6) Вывод на экран элементов множества в порядке возрастания
-def sort(List):
-    L = List
-    N = len(List)
-    for i in range(N-1):
-        for j in range(N-i-1):
-            if L[j] > L[j+1]:
-                L[j], L[j+1] = L[j+1], L[j]
-    return L
-    
-print(sort([90,2,1,34,65,22,12]))
+def sort(list,cur,pr):
+    if(cur == len(list)-1):
+        pr = pr + 1
+    elif(list[cur] > list[cur+1]):
+        list[cur],list[cur+1] = list[cur+1],list[cur]
+        sort(list,cur+1,pr)
+    elif(list[cur] <= list[cur+1]):
+        sort(list,cur+1,pr)
+    if(pr < len(list)-1):
+        sort(list,pr+1,pr+1)
+    return list
 
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+L3 = [2,3,8,9,6]
+print(sort(L3,0,0))
 
